421eac2a361210bb64b6deed504ef1fa4f676eba
Amit Bhattacharya <amit.bhattacharya2002@gmail.com>
2022-04-12 15:54:31 -0700

    browseLibraryPage added

--- public/libCatPageMod.js
+++ public/libCatPageMod.js
@@ -33,6 +33,13 @@ const libraryCatalog = [
         author: 'JK Rowling',
         desc: 'Harry Potter a wizard by birth explores the whole new world of magic on his 11th birthday',
         url: 'https://embed.cdn.pais.scholastic.com/v1/channels/sso/products/identifiers/isbn/9780590353403/primary/renditions/700'
+      },
+      {
+        id: '6',
+        name: 'Harry Potter and the Sorcerers Stone',
+        author: 'JK Rowling',
+        desc: 'Harry Potter a wizard by birth explores the whole new world of magic on his 11th birthday',
+        url: 'https://embed.cdn.pais.scholastic.com/v1/channels/sso/products/identifiers/isbn/9780590353403/primary/renditions/700'
       }
   ]
 const Books = (props) => {
@@ -44,6 +51,11 @@ const Books = (props) => {
         <h2 className="bookTitle">{props.title}</h2>
         <p>{props.author}</p>
         {/* <p>Return by : {props.retDate}</p> */}
+        <p>{props.desc}</p>
+        <div className="bookAction">
+            <a href=""><p>Borrow Book</p></a>
+            <a href=""><p>View Details</p></a>
+        </div>
         
       </div>
     )
@@ -54,7 +66,7 @@ const Books = (props) => {
       <div className="container">
   
         {libraryCatalog.map(books => (
-          <Books title={books.name} imgurl={books.url} author={books.author} retDate={books.returnDate}/>)
+          <Books title={books.name} imgurl={books.url} author={books.author} retDate={books.returnDate} desc={books.desc}/>)
         )}
   
       </div>
